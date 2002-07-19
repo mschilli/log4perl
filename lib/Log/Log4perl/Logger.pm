71e3951e75893c7cb3b62ee8e3d30ef748f57ba0
kgoess <kgoess>
2002-07-19 19:14:27 +0000

    big refactoring for speed

--- lib/Log/Log4perl/Logger.pm
+++ lib/Log/Log4perl/Logger.pm
@@ -11,12 +11,13 @@ use Log::Log4perl::Layout;
 use Log::Log4perl::Appender;
 use Log::Dispatch;
 use Carp;
+use Data::Dump qw(dump);
 
     # Initialization
 our $ROOT_LOGGER;
 our $LOGGERS_BY_NAME;
-our %LAYOUT_BY_APPENDER;
 our %APPENDER_BY_NAME = ();
+
 our $DISPATCHER = Log::Dispatch->new();
 
 __PACKAGE__->reset();
@@ -33,15 +34,15 @@ sub init {
 sub reset {
 ##################################################
     our $ROOT_LOGGER        = __PACKAGE__->_new("", $DEBUG);
-    our $DISPATCHER         = Log::Dispatch->new();
     our $LOGGERS_BY_NAME    = {};
-    our %APPENDER_BY_NAME   = ();
+    our $DISPATCHER = Log::Dispatch->new();
 }
 
 ##################################################
 sub _new {
 ##################################################
     my($class, $category, $level) = @_;
+    
 
     die "usage: __PACKAGE__->_new(category)" unless
         defined $category;
@@ -54,7 +55,7 @@ sub _new {
     }
 
     my $self  = {
-        logger_class  => $category,
+        category  => $category,
         num_appenders => 0,
         additivity    => 1,
         level         => $level,
@@ -62,12 +63,107 @@ sub _new {
         layout        => undef,
                 };
 
+   bless $self, $class;
+
+   $level ||= $self->level();
+
         # Save it in global structure
-    $LOGGERS_BY_NAME->{$category} = $self;
+   $LOGGERS_BY_NAME->{$category} = $self;
 
-    bless $self, $class;
+   $self->set_output_methods;
 
-    return $self;
+   return $self;
+}
+
+##################################################
+sub reset_all_output_methods {
+##################################################
+    foreach my $loggername ( keys %$LOGGERS_BY_NAME){
+        $LOGGERS_BY_NAME->{$loggername}->set_output_methods;
+    }
+    $ROOT_LOGGER->set_output_methods;
+}
+
+##################################################
+sub set_output_methods {
+# Here's a big performance increase.  Instead of having the logger
+# calculate whether to log and whom to log to every time log() is called,
+# we calculcate it once when the logger is created, and recalculate
+# it if the config information ever changes.
+#
+##################################################
+   my ($self) = @_;
+    
+
+   my (@appenders, %seen);
+
+   my ($level) = $self->level();
+
+   #collect the appenders in effect for this category    
+
+   for(my $logger = $self; $logger; $logger = parent_logger($logger)) {
+
+        foreach my $appender_name (@{$logger->{appender_names}}){
+
+                #only one message per appender, please
+            next if $seen{$appender_name} ++;
+
+            push (@appenders,     
+                   [$appender_name,
+                    $APPENDER_BY_NAME{$appender_name},
+                   ]
+            );
+        }
+        last unless $logger->{additivity};
+    }
+
+        #make a no-op coderef for inactive levels
+    my $noop = sub {};
+
+       #make a coderef
+    my $coderef = (! @appenders ? $noop : 
+        sub {
+          my ($logger) = shift;
+          my ($message) = shift;
+          my ($level) = shift;
+          foreach my $a (@appenders) {   #note the closure here
+              my ($appender_name, $appender) = @$a;
+
+              $appender->log(
+                  #these get passed through to Log::Dispatch
+                  { name    => $appender_name,
+                    level   => 0,   
+                    message => $message,
+                  },
+                  #these we need
+                  $logger->{category},
+                  $level,
+              );
+
+          } #end foreach appenders
+
+        } #end coderef
+
+    ); #end assignment via ternary
+
+
+    #our %PRIORITY = (
+    # "FATAL" => 0,
+    # "ERROR" => 3,
+    # "WARN"  => 4,
+    # "INFO"  => 6,
+    # "DEBUG" => 7,
+    #);
+
+    my %priority = %Log::Log4perl::Level::PRIORITY; #convenience and cvs
+
+    foreach my $levelname (keys %priority){
+        if ($priority{$levelname} <= $level) {
+            $self->{$levelname} = $coderef;
+        }else{
+            $self->{$levelname} = $noop;
+        }
+    }
 }
 
 ##################################################
@@ -93,13 +189,18 @@ sub parent_string {
 ##################################################
 sub level {
 ##################################################
-    my($self, $level) = @_;
+    my($self, $level, $dont_reset_all) = @_;
 
         # 'Set' function
     if(defined $level) {
         croak "invalid level '$level'" 
                 unless Log::Log4perl::Level::is_valid($level);
         $self->{level} = $level;   
+
+        &reset_all_output_methods
+            unless $dont_reset_all;  #keep us from getting overworked 
+                                     #if it's the config file calling us 
+
         return $level;
     }
 
@@ -112,13 +213,13 @@ sub level {
 
         # Does the current logger have the level defined?
 
-        if($logger->{logger_class} eq "") {
+        if($logger->{category} eq "") {
             # It's the root logger
             return $ROOT_LOGGER->{level};
         }
             
-        if(defined $LOGGERS_BY_NAME->{$logger->{logger_class}}->{level}) {
-            return $LOGGERS_BY_NAME->{$logger->{logger_class}}->{level};
+        if(defined $LOGGERS_BY_NAME->{$logger->{category}}->{level}) {
+            return $LOGGERS_BY_NAME->{$logger->{category}}->{level};
         }
     }
 
@@ -134,13 +235,13 @@ sub parent_logger {
     my($logger) = @_;
 
         # Is it the root logger?
-    if($logger->{logger_class} eq "") {
+    if($logger->{category} eq "") {
         # Root has no parent
         return undef;
     }
 
         # Go to the next defined (!) parent
-    my $parent_class = parent_string($logger->{logger_class});
+    my $parent_class = parent_string($logger->{category});
 
     while($parent_class ne "" and
           ! exists $LOGGERS_BY_NAME->{$parent_class}) {
@@ -179,39 +280,49 @@ sub additivity {
 ##################################################
 sub get_logger {
 ##################################################
-    my($class, $logger_class) = @_;
+    my($class, $category) = @_;
 
     unless(defined $ROOT_LOGGER) {
         die "Logger not initialized. No previous call to init()?";
     }
 
-    return $ROOT_LOGGER if $logger_class eq "";
+    return $ROOT_LOGGER if $category eq "";
 
-    my $logger = $class->_new($logger_class);
+    my $logger = $class->_new($category);
     return $logger;
 }
 
 ##################################################
 sub add_appender {
 ##################################################
-    my($self, $appender, $not_to_dispatcher) = @_;
+    my($self, $appender, $dont_reset_all) = @_;
+
+    my $not_to_dispatcher = 0;
 
     my $appender_name = $appender->name();
 
-    $self->{num_appenders}++;
+    $self->{num_appenders}++;  #should this be inside the unless?
 
     unless (grep{$_ eq $appender_name} @{$self->{appender_names}}){
         $self->{appender_names} = [sort @{$self->{appender_names}}, 
                                         $appender_name];
     }
 
-    #ugly, but while we want to track the names of
-    #all the appenders in a category, we only
-    #want to add it to log_dispatch *once*
-    $self->{dispatcher}->add($appender)
-        unless $APPENDER_BY_NAME{$appender_name};
+    if ($APPENDER_BY_NAME{$appender_name}) {
+        $not_to_dispatcher = 1;
+    }else{
+        $APPENDER_BY_NAME{$appender_name} = $appender;
+    }
 
-    $APPENDER_BY_NAME{$appender_name} = $appender;
+    &reset_all_output_methods
+                unless $dont_reset_all;  # keep us from getting overworked
+                                         # if it's  the config file calling us
+
+
+    $self->{dispatcher}->add($appender) unless $not_to_dispatcher;    
+        # while we want to track the names of
+        # all the appenders in a category, we only
+        # want to add it to log_dispatch *once*
 }
 
 ##################################################
@@ -226,75 +337,40 @@ sub has_appenders {
 sub log {
 # external api
 ##################################################
-    my ($self, $priority, @message) = @_;
+    my ($self, $priority, $message) = @_;
 
     croak "priority $priority isn't numeric" if ($priority =~ /\D/);
 
-    _log ($self, Log::Log4perl::Level::to_level($priority), $priority, @message);
+    my $which = Log::Log4perl::Level::to_level($priority);
+
+    $self->{$which}($self, $message, Log::Log4perl::Level::to_level($priority));
 
 }
 
 
 ##################################################
-sub _log {
-# internal implementation of $logger->debug(...), etc.
-##################################################
-    my($self, $level, $priority, @message) = @_;
-
-    my %seen;
-
-    my $message = join '', @message;
-
-    my $category = $self->{logger_class};
-
-    if($priority <= $self->level()) {
-        # Call the dispatchers up the hierarchy
-        for(my $logger = $self; $logger; $logger = parent_logger($logger)) {
-
-               # Only format the message if there's going to be an appender.
-            next unless $logger->has_appenders();
-
-            foreach my $appender_name (@{$logger->{appender_names}}){
-
-                    #only one message per appender, please
-                next if $seen{$appender_name} ++;
-
-                my $appender = $APPENDER_BY_NAME{$appender_name};
-
-                my $rendered_msg;
-
-                if ($appender->layout()) {
-                    $rendered_msg = $appender->layout()->render(
-                            $logger, $message, $category,
-                            $level, 2);  # 2 levels up from the renderer
-                                         # is the function 
-                                         # calling into the Logger
-                }else{
-                    # Accoding to 
-                    # http://jakarta.apache.org/log4j/docs/api/org/...
-                    # apache/log4j/SimpleLayout.html this is the default layout
-                    # TODO: Replace with SimpleFormat
-                    $rendered_msg = "$level - $message";
-                }
-
-                    # Dispatch the (formatted) message
-                $logger->{dispatcher}->log_to(
-                    name    => $appender_name,
-                    level   => lc(Log::Log4perl::Level::to_LogDispatch_string($priority)),
-                    message => $rendered_msg,
-                    );
-            }
-            last unless $logger->{additivity};
-        }
-    }
+#expected args are $logger, $msg, $levelname
+sub fatal {
+   $_[0]->{FATAL}(@_, 'FATAL');
+}
+sub error {
+   $_[0]->{ERROR}(@_, 'ERROR');
+}
+sub warn {
+   $_[0]->{WARN} (@_, 'WARN' );
+}
+sub info {
+   $_[0]->{INFO} (@_, 'INFO' );
+}
+sub debug {
+   $_[0]->{DEBUG}(@_, 'DEBUG');
 }
 
-##################################################
-sub debug { &_log($_[0], 'DEBUG', $DEBUG, @_[1,]); }
-sub info  { &_log($_[0], 'INFO',  $INFO,  @_[1,]); }
-sub warn  { &_log($_[0], 'WARN',  $WARN,  @_[1,]); }
-sub error { &_log($_[0], 'ERROR', $ERROR, @_[1,]); }
-sub fatal { &_log($_[0], 'FATAL', $FATAL, @_[1,]); }
+#sub debug { &log($_[0], 'DEBUG', $DEBUG, @_[1,]); }
+#sub info  { &log($_[0], 'INFO',  $INFO,  @_[1,]); }
+#sub warn  { &log($_[0], 'WARN',  $WARN,  @_[1,]); }
+#sub error { &log($_[0], 'ERROR', $ERROR, @_[1,]); }
+#sub fatal { &log($_[0], 'FATAL', $FATAL, @_[1,]); }
 
 sub is_debug { return $_[0]->level() >= $DEBUG; }
 sub is_info  { return $_[0]->level() >= $INFO; }
@@ -315,54 +391,10 @@ Log::Log4perl::Logger - Main Logger
 
   use Log::Log4perl::Logger;
 
-      # Init it only once
-  Log::Log4perl::Logger->init();
-
-      # Obtain an instance of a logger (many times)
-      # (_new() is not used externally because of singleton
-      #  behaviour)
-  my $logger = Log::Log4perl::Logger->get_logger($component);
-
-      # Add an appender to the logger
-  Log::Log4perl::Logger->add_appender($appender);
-
-      # re-init to delete all previously defined loggers
-  Log::Log4perl::Logger->reset();
-
-      # Log if logger's level is $level or higher
-  Log::Log4perl::Logger->log($level, $message);
-
-      # Log if logger's level is DEBUG or higher
-  Log::Log4perl::Logger->debug($message);
-      # Log if logger's level is DEBUG or higher
-  Log::Log4perl::Logger->info($message);
-      # Log if logger's level is DEBUG or higher
-  Log::Log4perl::Logger->warn($message);
-      # Log if logger's level is DEBUG or higher
-  Log::Log4perl::Logger->error($message);
-      # Log if logger's level is DEBUG or higher
-  Log::Log4perl::Logger->fatal($message);
-
-      # True if logger's level is DEBUG or higher
-  Log::Log4perl::Logger->is_debug();
-      # True if logger's level is INFO or higher
-  Log::Log4perl::Logger->is_info();
-      # True if logger's level is WARN or higher
-  Log::Log4perl::Logger->is_warn();
-      # True if logger's level is ERROR or higher
-  Log::Log4perl::Logger->is_error();
-      # True if logger's level is FATAL
-  Log::Log4perl::Logger->is_fatal();
+  my $log =  Log::Log4perl::Logger();
+  $log->debug("Debug Message");
 
 =head1 DESCRIPTION
-
-C<Log::Log4perl::Logger> is the main logger class. It provides the
-method C<get_logger($compontent)> which obtains a 
-logger. This is different from I<creating> a logger because
-loggers are only created once and then this single one instance is
-used all over the system. For this reason, there's no C<new()>
-method (there's an internal-only method called C<_new()> just in
-case you're curious).
 
 =head1 SEE ALSO
 
