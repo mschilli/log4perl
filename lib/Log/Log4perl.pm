d7bea4b593aa6412887d8677676f828ec229b13a
mschilli <mschilli>
2003-02-03 18:46:32 +0000

    0.30dev

--- lib/Log/Log4perl.pm
+++ lib/Log/Log4perl.pm
@@ -14,7 +14,7 @@ use Log::Log4perl::Appender;
 
 use constant DEBUG => 1;
 
-our $VERSION = '0.30';
+our $VERSION = '0.30dev';
 
    # set this to '1' if you're using a wrapper
    # around Log::Log4perl
