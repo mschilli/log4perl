d4afdd13beffcddafa2c7ab164601165287efafc
mschilli <mschilli>
2003-10-17 03:27:21 +0000

    included re-connect

--- lib/Log/Log4perl/Appender/Socket.pm
+++ lib/Log/Log4perl/Appender/Socket.pm
@@ -14,25 +14,54 @@ sub new {
 
     my $self = {
         name      => "unknown name",
-        type      => SOCK_STREAM,
-        timeout   => 5,
+        PeerAddr  => "localhost",
+        Proto     => 'tcp',
+        Timeout   => 5,
         @options,
     };
 
-    $self->{socket} = IO::Socket::INET->new(@options);
+    bless $self, $class;
+
+    unless($self->connect(@options)) {
+        die "Connect to $self->{PeerAddr}:$self->{PeerPort} failed: $!";
+    }
+
     $self->{socket}->autoflush(1);
 
-    bless $self, $class;
+    return $self;
 }
     
+##################################################
+sub connect {
+##################################################
+    my($self, @options) = @_;
+
+    $self->{socket} = IO::Socket::INET->new(@options);
+
+    return $self->{socket};
+}
+
 ##################################################
 sub log {
 ##################################################
     my($self, %params) = @_;
 
-    $self->{socket}->send($params{message});
-    $self->{socket}->flush();
+    {
+        eval { $self->{socket}->send($params{message}); 
+             };
 
+        if($@) {
+            warn "Send to " . ref($self) . " failed ($@)";
+            if($self->connect(%$self)) {
+                redo;
+            }
+            warn "Reconnect to $self->{PeerAddr}:$self->{PeerPort} " .
+                 "failed: $!";
+            return undef;
+        }
+    };
+
+    return 1;
 }
 
 ##################################################
@@ -55,12 +84,12 @@ Log::Log4perl::Appender::Socket - Log to a socket
 
     use Log::Log4perl::Appender::Socket;
 
-    my $app = Log::Log4perl::Appender::Socket->new(
-      PeerHost => "server.foo.com",
+    my $appender = Log::Log4perl::Appender::Socket->new(
+      PeerAddr => "server.foo.com",
       PeerPort => 1234,
     );
 
-    $file->log(message => "Log me\n");
+    $appender->log(message => "Log me\n");
 
 =head1 DESCRIPTION
 
@@ -69,6 +98,55 @@ L<IO::Socket::INET> and offers all parameters this module offers.
 
 Upon destruction of the object, pending messages will be flushed
 and the socket will be closed.
+
+If the appender cannot contact the server during the initialization
+phase (while running the constructor C<new>), it will C<die()>.
+
+If the appender fails to log a message because the socket's C<send()>
+method fails (most likely because the server went down), it will
+try to reconnect once. If it succeeds, the message will be sent.
+If the reconnect fails, a warning is sent to STDERR and the C<log()>
+method returns, discarding the message.
+
+=head1 EXAMPLE
+
+Write a server quickly using the IO::Socket::INET module:
+
+    use IO::Socket::INET;
+
+    my $sock = IO::Socket::INET->new(
+        Listen    => 5,
+        LocalAddr => 'localhost',
+        LocalPort => 12345,
+        Proto     => 'tcp');
+
+    while(my $client = $sock->accept()) {
+        print "Client connected\n";
+        while(<$client>) {
+            print "$_\n";
+        }
+    }
+
+Start it and then run the following script as a client:
+
+    use Log::Log4perl qw(:easy);
+
+    my $conf = q{
+        log4perl.category                  = WARN, Socket
+        log4perl.appender.Socket           = Log::Log4perl::Appender::Socket
+        log4perl.appender.Socket.PeerAddr  = localhost
+        log4perl.appender.Socket.PeerPort  = 12345
+        log4perl.appender.Socket.layout    = SimpleLayout
+    };
+
+    Log::Log4perl->init(\$conf);
+
+    sleep(2);
+
+    for(1..10) {
+        ERROR("Quack!");
+        sleep(5);
+    }
 
 =head1 AUTHOR
 
