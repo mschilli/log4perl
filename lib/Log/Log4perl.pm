3a9a49f92227a3fc0a230990b0fe347fda73dc70
mschilli <mschilli>
2005-01-30 03:59:51 +0000

    0.52

--- lib/Log/Log4perl.pm
+++ lib/Log/Log4perl.pm
@@ -16,7 +16,7 @@ use Log::Log4perl::Appender;
 
 use constant _INTERNAL_DEBUG => 1;
 
-our $VERSION = '0.51';
+our $VERSION = '0.52';
 
    # set this to '1' if you're using a wrapper
    # around Log::Log4perl
