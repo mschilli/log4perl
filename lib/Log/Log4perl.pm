825fc4ca74b50157be0fc8f9004099afd41ca1d2
mschilli <mschilli>
2008-02-10 08:07:05 +0000

    bumped version number

--- lib/Log/Log4perl.pm
+++ lib/Log/Log4perl.pm
@@ -16,7 +16,7 @@ use Log::Log4perl::Appender;
 
 use constant _INTERNAL_DEBUG => 1;
 
-our $VERSION = '1.14';
+our $VERSION = '1.15';
 
    # set this to '1' if you're using a wrapper
    # around Log::Log4perl
