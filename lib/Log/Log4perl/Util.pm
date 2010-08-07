189e67b0bc69556210052ec74f1fed703aca6d33
mschilli <github@perlmeister.com>
2010-08-06 21:24:31 -0700

    [RT 24884] Using require() instead of incomplete logic in L4p::Util::module_available(). local __DIE__ handler takes care of user-defined __DIE__ handlers ignoring $^S (suggested by Eric Wilhelm and others).

--- lib/Log/Log4perl/Util.pm
+++ lib/Log/Log4perl/Util.pm
@@ -5,41 +5,23 @@ use File::Spec;
 ##################################################
 sub module_available {  # Check if a module is available
 ##################################################
-# This has to be here, otherwise the following 'use'
-# statements will fail.
-##################################################
     my($full_name) = @_;
 
-    my $relpath = File::Spec->catfile(split /::/, $full_name) . '.pm';
+      # Weird cases like "strict;" (including the semicolon) would 
+      # succeed with the eval below, so check those up front. 
+      # I can't believe Perl doesn't have a proper way to check if a 
+      # module is available or not!
+    return 0 if $full_name =~ /[^\w:]/;
 
-        # Work around a bug in Activestate's "perlapp", which uses
-        # forward slashes instead of Win32 ones.
-    my $relpath_with_forward_slashes = 
-        join('/', (split /::/, $full_name)) . '.pm';
+    local $SIG{__DIE__} = sub {};
 
-    return 1 if exists $INC{$relpath} or
-                exists $INC{$relpath_with_forward_slashes};
-    
-    foreach my $dir (@INC) {
-        if(ref $dir) {
-            # This is fairly obscure 'require'-functionality, nevertheless
-            # trying to implement them as diligently as possible. For
-            # details, check "perldoc -f require".
-            if(ref $dir eq "CODE") {
-                return 1 if $dir->($dir, $relpath_with_forward_slashes);
-            } elsif(ref $dir eq "ARRAY") {
-                return 1 if $dir->[0]->($dir, $relpath_with_forward_slashes);
-            } elsif(ref $dir and 
-                    ref $dir !~ /^(GLOB|SCALAR|HASH|REF|LVALUE)$/) {
-                return 1 if $dir->INC();
-            }
-        } else {
-            # That's the regular case
-            return 1 if -r File::Spec->catfile($dir, $relpath);
-        }
+    eval "require $full_name";
+
+    if($@) {
+        return 0;
     }
-              
-    return 0;
+
+    return 1;
 }
 
 ##################################################
