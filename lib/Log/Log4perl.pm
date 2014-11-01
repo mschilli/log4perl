550d1d01e7c283eff4defbdc2b978c90a1728f9d
mschilli <github@perlmeister.com>
2014-10-31 18:46:28 -0700

    release 1.46

--- lib/Log/Log4perl.pm
+++ lib/Log/Log4perl.pm
@@ -14,7 +14,7 @@ use Log::Log4perl::Level;
 use Log::Log4perl::Config;
 use Log::Log4perl::Appender;
 
-our $VERSION = '1.45';
+our $VERSION = '1.46';
 
    # set this to '1' if you're using a wrapper
    # around Log::Log4perl
