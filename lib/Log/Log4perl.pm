51e6cfd76a8357a89f445268fda78e6196e9f6e0
Ed J <mohawk2@users.noreply.github.com>
2022-09-02 18:48:14 +0100

    1.56

--- lib/Log/Log4perl.pm
+++ lib/Log/Log4perl.pm
@@ -16,7 +16,7 @@ use Log::Log4perl::Level;
 use Log::Log4perl::Config;
 use Log::Log4perl::Appender;
 
-our $VERSION = '1.55';
+our $VERSION = '1.56';
 
    # set this to '1' if you're using a wrapper
    # around Log::Log4perl
